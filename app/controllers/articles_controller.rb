0cd1970e0d51e4b8c9e13325544fa6fbb7353e4c
Karan Kumar <karankumar.world@gmail.com>
2016-04-09 23:00:01 +0530

    adding articles and showing them

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
 class ArticlesController < ApplicationController
-def new
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    puts @article
+
+    @article.save
+    redirect_to @article
   end
 end
