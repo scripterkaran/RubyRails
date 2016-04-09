f00d19051c58bb149fcc1e9f9aeb05f6527530d2
Karan Kumar <karankumar.world@gmail.com>
2016-04-09 22:24:42 +0530

    routes to new articles view

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ RailsApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  resources :articles
+
   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
