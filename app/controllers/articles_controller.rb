8a636d7b8e9b25236f5c57f256ee0fb4aecb59aa
Karan Kumar <karankumar.world@gmail.com>
2016-04-10 01:09:57 +0530

    update method broken , delete method , cleaning up of form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,41 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params[:article])
     puts @article
-
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def index
      @articles = Article.all
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+  @article = Article.find(params[:id])
+      if @article.update(params[:article])
+        redirect_to @article
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+  end
+
+
 end
